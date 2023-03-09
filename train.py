fe60d36fb3e46ee1b558fe1f44e1520916fc2255
alexmacharia <alex.machariam@gmail.com>
2023-03-09 21:37:39 +0300

    upload files

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 training_data_df = pd.read_csv("sales_data_training.csv", dtype=float)
 
 # Separate X and Y datasets
-X_training = training_data_df.drop('total_earnings', axis=1)
+X_training = training_data_df.drop('total_earnings', axis=1).values
 y_training = training_data_df[['total_earnings']].values
 
 # Load testing data from csv file
@@ -30,4 +30,18 @@ y_scaled_training = y_scaler.fit_transform(y_training)
 
 # Use same scaler to scale test data
 X_scaled_testing = X_scaler.transform(X_testing)
-y_scaled_testing = y_caler.transform(y_testing)
+y_scaled_testing = y_scaler.transform(y_testing)
+
+# Define model parameters
+learning_rate = 0.001
+training_epochs = 100
+
+# Define inputs and outputs
+number_of_inputs = 9
+number_of_outputs = 1
+
+# Define number of neurons in each layer
+layer_1_nodes = 50
+layer_2_nodes = 100
+layer_3_nodes = 50
+
