1c74b7cd6368a961ab92fd645b7509026a7f2b18
alexmacharia <alex.machariam@gmail.com>
2023-03-09 21:29:15 +0300

    upload files

--- train.py
+++ train.py
@@ -15,3 +15,19 @@ y_training = training_data_df[['total_earnings']].values
 
 # Load testing data from csv file
 test_data_df = pd.read_csv("sales_data_test.csv", dtype=float)
+
+# Separate X and y columns
+X_testing = test_data_df.drop('total_earnings', axis=1).values
+y_testing = test_data_df[['total_earnings']].values
+
+# Scale the data to 0 to 1 range
+X_scaler = MinMaxScaler(feature_range=(0,1))
+y_scaler = MinMaxScaler(feature_range=(0,1))
+
+# Scale both training features and labels
+X_scaled_training = X_scaler.fit_transform(X_training)
+y_scaled_training = y_scaler.fit_transform(y_training)
+
+# Use same scaler to scale test data
+X_scaled_testing = X_scaler.transform(X_testing)
+y_scaled_testing = y_caler.transform(y_testing)
